bcf73fdc98bb832bae7c21d56c36fbba2dac8f06
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-21 14:52:14 +0100

    consegna snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,11 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+const authors = posts.map((author) => {
+  return author.author;
+  
+});
+
+console.log(authors);
