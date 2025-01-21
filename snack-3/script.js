ac7a97cd71f7e3b48595e71d49f3594e1df27513
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-21 14:50:20 +0100

    consegna esercizio 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbersPlus = numbers.map((numPlus) => {
+    return numPlus += 1;
+    
+});
+
+console.log(numbersPlus);
